7e8714f23776ede03aa1fd5ddbd6be68c9f6263f
widi9545 <widi9545@colorado.edu>
2024-12-10 02:15:46 -0700

    added finder app

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,11 +6,11 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '9f0de1a'
+AESD_ASSIGNMENTS_VERSION = 'ff43936'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-widi9545.git'
+AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignment-5-widi9545.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 CROSS_COMPILE=aarch64-none-linux-gnu-
---
+++ finder-app/manual-linux.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+# Script outline to install and build kernel.
+# Author: Siddhant Jajoo.
+
+set -e
+set -u
+
+OUTDIR=/tmp/aeld
+KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
+KERNEL_VERSION=v5.15.163
+BUSYBOX_VERSION=1_33_1
+FINDER_APP_DIR=$(realpath $(dirname $0))
+ARCH=arm64
+CROSS_COMPILE=aarch64-none-linux-gnu-
+
+if [ $# -lt 1 ]
+then
+	echo "Using default directory ${OUTDIR} for output"
+else
+	OUTDIR=$1
+	echo "Using passed directory ${OUTDIR} for output"
+fi
+
+mkdir -p /tmp/aeld
+
+cd /tmp/aeld
+if [ ! -d "/tmp/aeld/linux-stable" ]; then
+    #Clone only if the repository does not exist.
+	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN /tmp/aeld"
+	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+fi
+if [ ! -e $/tmp/aeld/linux-stable/arch/${ARCH}/boot/Image ]; then
+    cd linux-stable
+    echo "Checking out version ${KERNEL_VERSION}"
+    git checkout ${KERNEL_VERSION}
+    #make ARCH=${ARCH} CROSS_COMPILE=$CROSS_COMPILE mrproper
+    make -j 9 ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE defconfig
+    make -j 9 ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE all
+fi
+
+echo "Adding the Image in outdir"
+
+
+echo "Creating the staging directory for the root filesystem"
+cd "/tmp/aeld"
+if [ -d "/tmp/aeld/rootfs" ]
+then
+	echo "Deleting rootfs directory at $/tmp/aeld/rootfs and starting over"
+    sudo rm  -rf /tmp/aeld/rootfs
+fi
+
+# TODO: Create necessary base directories
+mkdir /tmp/aeld/rootfs
+cd /tmp/aeld/rootfs
+mkdir -p bin dev etc home lib lib64 root proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log 
+
+echo "this is the working directory $PWD"
+echo "this is the hostname echo $HOSTNAME"
+echo "this is where the image is "
+cp /tmp/aeld/linux-stable/arch/arm64/boot/Image /tmp/aeld/rootfs
+echo "this is where the image is "
+ln -s /tmp/aeld/rootfs /tmp/aesd-autograder
+echo "this is after looking at the autograder `ls /tmp/aesd-autograder`"
+echo "this is the sysroot docker"
+${CROSS_COMPILE}gcc -print-sysroot
+
+
+
+cp /usr/local/arm-cross-compiler/install/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/bin/../aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 /tmp/aeld/rootfs/lib64
+cp /usr/local/arm-cross-compiler/install/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/bin/../aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 /tmp/aeld/rootfs/lib
+cp /usr/local/arm-cross-compiler/install/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/bin/../aarch64-none-linux-gnu/libc/lib64/libm.so.6 /tmp/aeld/rootfs/lib64
+cp /usr/local/arm-cross-compiler/install/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/bin/../aarch64-none-linux-gnu/libc/lib64/libm.so.6 /tmp/aeld/rootfs/lib
+cp /usr/local/arm-cross-compiler/install/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/bin/../aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 /tmp/aeld/rootfs/lib64
+cp /usr/local/arm-cross-compiler/install/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/bin/../aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 /tmp/aeld/rootfs/lib
+cp /usr/local/arm-cross-compiler/install/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/bin/../aarch64-none-linux-gnu/libc/lib64/libc.so.6 /tmp/aeld/rootfs/lib64
+cp /usr/local/arm-cross-compiler/install/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/bin/../aarch64-none-linux-gnu/libc/lib64/libc.so.6 /tmp/aeld/rootfs/lib
+
+
+
+cd "/tmp/aeld"
+if [ ! -d "/tmp/aeld/busybox" ]
+then
+git clone git://busybox.net/busybox.git
+    cd busybox
+    git checkout ${BUSYBOX_VERSION}
+    make distclean
+    make defconfig
+else
+    cd busybox
+fi
+
+# TODO: Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} 
+make CONFIG_PREFIX=/tmp/aeld/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+
+cd /tmp/aeld/rootfs
+
+
+
+echo "Library dependencies"
+${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+
+
+
+# TODO: Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 622 dev/tty1 c 4 2 
+sudo mknod -m 622 dev/console c 5 1 
+
+# TODO: Clean and build the writer utility
+cd $FINDER_APP_DIR
+cp writer.c /tmp/aeld/rootfs/home
+cp start-qemu-terminal.sh /tmp/aeld/rootfs/home
+cp start-qemu-app.sh /tmp/aeld/rootfs/home
+cp autorun-qemu.sh /tmp/aeld/rootfs/home
+
+cp -r conf /tmp/aeld/rootfs/home
+cp finder-test.sh /tmp/aeld/rootfs/home
+cp finder.sh /tmp/aeld/rootfs/home
+
+chmod -R 777 /tmp/aeld/rootfs/home
+
+cd /tmp/aeld/rootfs/home
+${CROSS_COMPILE}gcc writer.c -o writer 
+
+# TODO: Chown the root directory
+cd /tmp/aeld/rootfs
+echo "we are here $PWD"
+
+sudo find . | sudo cpio -H newc -ov --owner root:root > initramfs.cpio
+# TODO: Create initramfs.cpio.gz
+gzip -f initramfs.cpio
