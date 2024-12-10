76b8380a5552762d515d9f44b3efd17f0d460a74
widi9545 <widi9545@colorado.edu>
2024-12-10 02:09:08 -0700

    adding server

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -13,9 +13,11 @@ AESD_ASSIGNMENTS_VERSION = 'a8a45f1'
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-widi9545.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+CROSS_COMPILE=aarch64-none-linux-gnu-
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(CROSS_COMPILE) -C $(@D)/server all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
@@ -30,6 +32,10 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/etc/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/etc/finder-app
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/bin
+
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket /usr/bin
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop.sh /etc/init.d/S99aesdsocket
 endef
 
 $(eval $(generic-package))
---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#https://unix.stackexchange.com/questions/20357/how-can-i-make-a-script-in-etc-init-d-start-at-boot
+
+
+start() {
+    start-stop-daemon -S -n aedsocket --exec /usr/bin/aedsocket 
+
+}
+   
+stop() {
+    start-stop-daemon --stop --signal SIGTERM --name aedsocket --retry 3 
+
+}
+
+restart(){
+    stop
+    sleep 10
+    start
+}
+   
+case "$1" in 
+    start)
+       start
+       ;;
+    stop)
+       stop
+       ;;
+    restart)
+        restart
+
+       ;;
+    *)
+       echo "Usage: $0 {start|stop|restart}"
+esac
+   
+exit 0 
