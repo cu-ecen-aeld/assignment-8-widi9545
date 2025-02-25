8fcc0764248222d4284b43bf21a4ae51c07eb62a
William Diment <widi9545@colorado.edu>
2025-02-25 06:51:40 +0000

    push

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -6,36 +6,16 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-LDD_VERSION = '24ea316'
+LDD_VERSION = '621f60a9f20fbf618356d63cc883dec825076eec'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-widi9545.git'
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
+LDD_MODULE_SUBDIRS = misc-modules
 
-define LDD_BUILD_CMDS
-	$(MAKE)  -C $(@D)/misc-modules 
-	$(MAKE)  -C $(@D)/scull 
-endef
-
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-define LDD_INSTALL_TARGET_CMDS
-
-	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/etc/misc-modules
-	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/etc/misc-modules
-
-	$(INSTALL) -m 0755 $(@D)/scull/scull.init $(TARGET_DIR)/etc/scull
-	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/etc/scull
-	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/etc/scull
-
-endef
-#	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
-
-
-
-
-
-
-
+#https://buildroot.org/downloads/manual/manual.html - 18.20
+$(eval $(kernel-module))
 $(eval $(generic-package))
+
